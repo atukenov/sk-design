f54ac6711df752916684e9120336c5162b2c0b1c
atukenov <almaz.t97@gmail.com>
2023-06-21 00:50:13 +0500

    image added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
   return (
     <div className="flex flex-wrap">
       <div className="w-full md:w-1/2 flex justify-center items-center">
-        photo
+        <Image src="/img/about.jpg" alt="me" width={500} height={500} />
       </div>
-      <div className="w-full md:w-1/2">about</div>
+      <div className="w-full md:w-1/2 flex justify-center items-center">
+        about
+      </div>
     </div>
   );
 };
