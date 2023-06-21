cb295d584df9df3490302ac8541031675f0eb159
Almaz Tukenov <almaztukenov@tengizchevroil.com>
2023-06-21 16:56:01 +0500

    slides added to "about me"

--- app/components/SK/index.tsx
+++ app/components/SK/index.tsx
@@ -1,13 +1,96 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
+const slides = [
+  {
+    title: "pervoye",
+    bg: "bg-red-300 w-[300px] h-[300px] rotate-[5deg]",
+  },
+  {
+    title: "vtoroe",
+    bg: "bg-blue-300  w-[300px] h-[300px] -rotate-[10deg]",
+  },
+  {
+    title: "tretye",
+    bg: "bg-green-300  w-[300px] h-[300px] rotate-[15deg]",
+  },
+  {
+    title: "chetvertoe",
+    bg: "bg-orange-300  w-[300px] h-[300px] -rotate-[20deg]",
+  },
+];
+
 const SK = () => {
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [wheel, setWheel] = useState<number>(0);
+
+  const [touchStart, setTouchStart] = useState<number | null>(null);
+  const [touchEnd, setTouchEnd] = useState<number | null>(null);
+  const minSwipeDistance = 5;
+
+  const onTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientY);
+  };
+
+  const onTouchMove = (e: React.TouchEvent<HTMLDivElement>) =>
+    setTouchEnd(e.targetTouches[0].clientY);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isDownSwipe = distance > minSwipeDistance;
+    const isUpSwipe = distance < -minSwipeDistance;
+    const newWheel = wheel + distance / 5;
+    console.log(wheel, distance);
+    if (isDownSwipe) {
+      console.log(wheel);
+    }
+    if (isUpSwipe) {
+      console.log(wheel);
+    }
+    setWheel(newWheel < 0 ? 0 : newWheel > 350 ? 350 : newWheel);
+  };
+
+  useEffect(() => {}, [wheel]);
+
+  const handleScroll = (e: React.WheelEvent<HTMLDivElement>) => {
+    const step = 5;
+    const newWheel = e.deltaY < 0 ? wheel - step : wheel + step;
+    setWheel(newWheel < 0 ? 0 : newWheel > 350 ? 350 : newWheel);
+  };
+
+  const renderStyle = (index: number) => {
+    if (index > wheel / 100)
+      return { right: `-${1500 * index - wheel * 17}px` };
+    return {};
+  };
+
   return (
-    <div className="flex flex-wrap flex-1">
+    <div
+      className="flex flex-wrap flex-1"
+      onWheel={handleScroll}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
       <div className="flex flex-col justify-center items-center w-full lg:w-1/3 z-0">
-        <div className="">pervoe</div>
-        <div>vtoroe</div>
+        <div className="flex flex-col justify-center items-center w-full h-full  relative">
+          {slides.map((slide, index) => {
+            return (
+              <div
+                key={index}
+                className={`absolute ${slide.bg} ${renderStyle(index)}`}
+                style={renderStyle(index)}
+              >
+                {slide.title}
+              </div>
+            );
+          })}
+        </div>
       </div>
+
       <div className="flex justify-end items-end w-full lg:w-2/3 relative -z-10">
         <div className="sk-font-s absolute text-[30rem] -bottom-[225px] right-[100px] sm:text-[45rem] sm:-bottom-[350px] sm:right-[175px] xl:text-[85rem] xl:-bottom-[650px] xl:right-[300px]">
           S
