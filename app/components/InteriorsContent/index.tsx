1b9215f7efb322ba11ae8f084716416583fcd0a2
amakenzi <almaz.t97@gmail.com>
2023-08-09 13:26:54 +0500

    content and gradient added

--- app/components/InteriorsContent/index.tsx
+++ app/components/InteriorsContent/index.tsx
@@ -1,7 +1,48 @@
+import Image from "next/image";
 import React from "react";
 
+const InteriorItems = [
+  {
+    project: "Проект ЖК Абырой",
+    desc: "",
+    theme: "",
+    src: "",
+  },
+  {
+    project: "Проект ЖК Талан, Алия",
+    desc: "",
+    theme: "",
+    src: "",
+  },
+  {
+    project: "Проект Отдел продаж BI Group",
+    desc: "",
+    theme: "",
+    src: "",
+  },
+  {
+    project: "Проект Жилой дом",
+    desc: "",
+    theme: "",
+    src: "",
+  },
+];
+
 const InteriorsContent = () => {
-  return <div>InteriorsContent</div>;
+  return (
+    <>
+      {InteriorItems.map((item, index) => {
+        return (
+          <div key={index}>
+            <p>{item.project}</p>
+            <p>{item.desc}</p>
+            <p>{item.theme}</p>
+            <Image src={item.src} alt="image" width={200} height={200} />
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default InteriorsContent;
--- app/components/UIContent/index.tsx
+++ app/components/UIContent/index.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import React from "react";
 
-const UI = () => {
-  return <div>UI</div>;
+const UIItems = [
+  {
+    project: 'Проект "Line Z"',
+    desc: "Сайт-портфолио для web, interior, graphic designes",
+    theme: "",
+    src: "",
+  },
+  {
+    project: 'Проект "Doctor Guw"',
+    desc: "Сайт для сбора рейтингов врачей г.Атырау",
+    theme: "",
+    src: "",
+  },
+];
+
+const UIContent = () => {
+  return (
+    <>
+      {UIItems.map((item, index) => {
+        return (
+          <div key={index}>
+            <p>{item.project}</p>
+            <p>{item.desc}</p>
+            <p>{item.theme}</p>
+            <Image src={item.src} alt="image" width={200} height={200} />
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
-export default UI;
+export default UIContent;
