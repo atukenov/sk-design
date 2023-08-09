ec8bd0cd776bca8aa6f38f4a40ff54066387095b
amakenzi <almaz.t97@gmail.com>
2023-08-09 16:57:00 +0500

    fonts and design added

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
   return (
     <main className="flex flex-1 justify-center overflow-hidden">
       <div className="flex flex-1 flex-col items-center">
-        <div className="flex flex-col items-center pt-10 relative mb-12">
-          <h1 className="text-5xl">PORTFOLIO</h1>
-          <h1 className="text-4xl text-red-600 absolute -bottom-5">Saltanat</h1>
-          <h1 className="text-4xl text-red-600 absolute -bottom-10">
+        <div className="flex flex-col items-center pt-10 relative mb-24">
+          <h1 className="text-5xl theseasons">PORTFOLIO</h1>
+          <h1 className="text-4xl text-red-600 absolute -bottom-[35px] halimun">
+            Saltanat
+          </h1>
+          <h1 className="text-4xl text-red-600 absolute -bottom-[85px] halimun">
             Kadirova
           </h1>
         </div>
