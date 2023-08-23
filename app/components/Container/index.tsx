ce3f52a5e82ac8a77c465ccfe45a9142b3747cde
atukenov <almaz.t97@gmail.com>
2023-08-24 00:56:56 +0500

    only mobile

--- app/components/Container/index.tsx
+++ app/components/Container/index.tsx
@@ -5,7 +5,13 @@ interface Props {
 }
 
 const Container: FC<Props> = ({ children }) => {
-  return <div className="px-6 py-4 h-full flex flex-col">{children}</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="px-6 py-4 h-full flex flex-col max-w-[500px] min-w-[500px]">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Container;
